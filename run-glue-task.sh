357ef0f90b60d15c89ffd538cf4f96770ed2deb0
Ali Zaidi <alikaz.zaidi@gmail.com>
2018-12-04 17:50:26 -0800

    ADD: script for single glue task

---
+++ run-glue-task.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+BERT_BASE_DIR='/home/alizaidi/nlpdev/BERT_implementations/pretrained-models/uncased_L-12_H-768_A-12'
+DATASETS='/home/alizaidi/data/glue_data/SST-2'
+OUTPUTS='/home/alizaidi/nlpdev/BERT_implementations/results/'
+TASK="SST"
+
+tensorboard --logdir $OUTPUTS &
+
+python run_classifier.py \
+  --task_name=$TASK \
+  --do_train=true \
+  --do_eval=true \
+  --data_dir=$DATASETS/$TASK \
+  --vocab_file=$BERT_BASE_DIR/vocab.txt \
+  --bert_config_file=$BERT_BASE_DIR/bert_config.json \
+  --init_checkpoint=$BERT_BASE_DIR/bert_model.ckpt \
+  --max_seq_length=256 \
+  --train_batch_size=16 \
+  --learning_rate=2e-5 \
+  --num_train_epochs=3 \
+  --output_dir=$OUTPUTS \
+  $@
